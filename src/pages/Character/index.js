48134c289b22109a8213bcbc4424ff097cd9913b
abdo <abdo.mohamadeen@gmail.com>
2022-01-30 17:05:53 +0400

    Handle case of no character found

--- src/pages/Character/index.js
+++ src/pages/Character/index.js
@@ -18,30 +18,32 @@ const Character = () => {
     <Container>
       {loading ? (
         <h2>Loading...</h2>
+      ) : !loading && !data?.character?.id ? (
+        <h2>No character found</h2>
       ) : (
-          <div>
-            <img src={data?.character?.image} alt='character' />
-            <h3>{data?.character?.name}</h3>
-            <h3>
-              Gender:{' '}
-              {data?.character?.gender?.toLowerCase() === 'male' ? (
-                <span role='img' aria-label='male' title='male'>
-                  👨
-                </span>
-              ) : (
-                <span role='img' aria-label='female' title='female'>
-                  👩
-                </span>
-              )}
-            </h3>
+        <div>
+          <img src={data?.character?.image} alt='character' />
+          <h3>{data?.character?.name}</h3>
+          <h3>
+            Gender:{' '}
+            {data?.character?.gender?.toLowerCase() === 'male' ? (
+              <span role='img' aria-label='male' title='male'>
+                👨
+              </span>
+            ) : (
+              <span role='img' aria-label='female' title='female'>
+                👩
+              </span>
+            )}
+          </h3>
 
-            <h3>Episodes:</h3>
-            {data?.character?.episode.map((episode) => (
-              <div>
-                {episode.name} <b>({episode.episode})</b>
-              </div>
-            ))}
-          </div>
+          <h3>Episodes:</h3>
+          {data?.character?.episode.map((episode) => (
+            <div>
+              {episode.name} <b>({episode.episode})</b>
+            </div>
+          ))}
+        </div>
       )}
     </Container>
   );
